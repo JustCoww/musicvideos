fa4260e0f0b44f662cfbda2391663345ca28808a
justcow <68345611+JustCoww@users.noreply.github.com>
2022-05-20 17:02:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,13 +11,12 @@ setup(
     author_email="<justcow@pm.me>",
     description=DESCRIPTION,
     packages=find_packages(),
-    install_requires=['moviepy', 'pydub', 'pedalboard', 'yt_dlp', 'pillow', 'soundfile', 'requests', 'google-api-python-client', 'google-auth-oauthlib', 'google-auth-httplib2'],
+    install_requires=['moviepy', 'pedalboard', 'pillow', 'soundfile'],
     keywords=['python', 'video', 'audio', 'justcow'],
     include_package_data=True ,
     classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
     ]
 )
