f3be46f1f11b4993690a7a8f2233b9a8f72b6a86
justcow <68345611+JustCoww@users.noreply.github.com>
2022-05-20 16:58:19 +0100

    Add files via upload

---
+++ musicvideos/video.py
@@ -0,0 +1,31 @@
+def exportvideo(image=None, audio=None, output='video.mp4'):
+    '''
+    This function will create a video
+    with the defined image and the defined audio.
+
+    Example:
+
+    export(audio='nightcore xXXi_wud_nvrstop_UUXXx.wav', image='video.png', outFile='videofile pagmann.mp4')
+
+    '''
+
+    logger.debug('Checking arguments...')
+    variables = [('image', image), ('audio', audio)]
+    for i in variables:
+        if i[1] is None:
+            print(f'{i[0]} is not defined')
+            return
+
+    # Import files
+    logger.info(f'Opening files "{audio}" "{image}"...')
+    audio = AudioFileClip(audio)
+    video = ImageClip(image)
+
+    # Mix into video
+    logger.debug(f'Setting up audio and video to export...')
+    video = video.set_audio(audio)
+    video.duration = audio.duration
+    video.fps = 1
+    logger.info(f'Exporting video to "{output}"...')
+    video.write_videofile(output)
+    return output
